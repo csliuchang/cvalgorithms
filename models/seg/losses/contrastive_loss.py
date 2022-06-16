6bff8cf871958db211f35d0f48b597fd687b8e8e
unknown <598306303@qq.com>
2022-06-16 17:53:14 +0800

    constrant loss

--- models/seg/losses/contrastive_loss.py
+++ models/seg/losses/contrastive_loss.py
@@ -4,30 +4,29 @@ from ...builder import LOSSES
 
 
 @LOSSES.register_module()
-class BBContrastiveLoss(nn.Module):
+class BatchContrastiveLoss(nn.Module):
     """
-    batch-balanced contrastive loss
+    pass
     """
 
     def __init__(self, margin=2.0):
-        super(BBContrastiveLoss, self).__init__()
+        super(BatchContrastiveLoss, self).__init__()
         self.margin = margin
 
     def forward(self, distance, label):
-        label[label == 1] = -1 # change
-        label[label == 0] = 1
-        mask = (label != 255).float()
+        losses = dict()
+        label_change = torch.zeros_like(label).to(label.device)
+        label_change[label == 1] = -1  # not similar
+        label_change[label == 0] = 1
+        mask = (label != 255)
         distance = distance * mask
-        pos_sum = torch.sum((label == 1).float()) + 0.0001
-        neg_sum = torch.sum((label == -1).float()) + 0.0001
-        # pos loss
-        loss_1 = torch.sum(
-            (1 + label) / 2 * torch.pow(distance, 2)
-        ) / pos_sum
-        # neg loss
-        loss_2 = torch.sum(
-            (1 - label) / 2 * mask *
-            torch.pow(torch.clamp(self.margin - distance, min=0.0), 2)
-        ) / neg_sum
-        loss = loss_1 + loss_2
-        return loss
+        pos_num = torch.sum((label_change == 1).float()) + 0.0001
+        neg_num = torch.sum((label_change == -1).float()) + 0.0001
+
+        loss_1 = torch.sum((1 + label_change) / 2 * torch.pow(distance, 2)) / pos_num
+        loss_2 = torch.sum((1 - label_change) / 2 * mask *
+                           torch.pow(torch.clamp(self.margin - distance, min=0.0), 2)
+                           ) / neg_num
+        losses['cnt_loss'] = loss_1 + loss_2
+        return losses
+
